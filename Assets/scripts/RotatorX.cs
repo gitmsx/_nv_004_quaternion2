45abf735bee7e8973100ae43b1ad88aa2e82c4d7
gitmsx <gitmsx@mail.ru>
2022-11-18 22:21:29 +0300

    Saved on 18.11.2022-22:21:28,55

---
+++ Assets/scripts/RotatorX.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RotatorX  : MonoBehaviour {
+	Quaternion originRotation;
+
+
+	float angle;
+	void Start () {
+		angle = 0;
+        originRotation=transform.rotation;
+
+
+    }
+
+	void FixedUpdate () {
+
+        angle = angle +0.1f;
+        Quaternion rotationX = Quaternion.AngleAxis(angle, Vector3.up);
+        
+
+        
+        transform.rotation = originRotation * rotationX;
+
+	
+	}
+}
---
+++ Assets/scripts/RotatorY.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RotatorY : MonoBehaviour {
+	Quaternion originRotation;
+
+
+	float angle;
+	void Start () {
+		angle = 0;
+        originRotation=transform.rotation;
+
+
+    }
+
+	void FixedUpdate () {
+
+        angle = angle +0.1f;
+        Quaternion rotationY = Quaternion.AngleAxis(angle, Vector3.up);
+        //Quaternion rotationX = Quaternion.AngleAxis(angle, Vector3.right);
+
+        //Quaternion rotationZ = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = originRotation * rotationY  ;
+
+	
+	}
+}
