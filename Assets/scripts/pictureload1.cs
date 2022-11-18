6924ec0acdd6970a72025363ea0ee6c374b7761e
gitmsx <gitmsx@mail.ru>
2022-11-18 14:48:14 +0300

    Saved on 18.11.2022-14:48:09,04

---
+++ Assets/scripts/pictureload1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pictureload1 : MonoBehaviour
+{
+
+    // Source texture and the rectangular area we want to extract.
+    public Texture2D sourceTex;
+    public Rect sourceRect;
+
+    void Start()
+    {
+        int x = Mathf.FloorToInt(sourceRect.x);
+        int y = Mathf.FloorToInt(sourceRect.y);
+        int width = Mathf.FloorToInt(sourceRect.width);
+        int height = Mathf.FloorToInt(sourceRect.height);
+
+        var  pix2 = sourceTex.GetPixel(x, y);
+        Color[] pix = sourceTex.GetPixels(x, y, width, height);
+
+        Texture2D destTex = new Texture2D(width, height);
+        destTex.SetPixels(pix);
+        destTex.Apply();
+
+        // Set the current object's texture to show the
+        // extracted rectangle.
+        GetComponent<Renderer>().material.mainTexture = destTex;
+    }
+
+
+
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
+
+
