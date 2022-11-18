35ba8a5f709c68735382f147e7b02278a4b0d41f
gitmsx <gitmsx@mail.ru>
2022-11-18 15:10:05 +0300

    Saved on 18.11.2022-15:10:04,09

---
+++ Assets/scripts/InitField2.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Color = UnityEngine.Color;
+
+
+
+public class InitField2 : MonoBehaviour
+{
+
+    private GameObject BoxClone;
+    public GameObject BulletPF;
+    // Start is called before the first frame update
+    public Texture2D sourceTex;
+    public Rect sourceRect;
+
+    void Start()
+    {
+
+        Fire();
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Fire()
+    {
+
+        int x = Mathf.FloorToInt(sourceRect.x);
+        int y = Mathf.FloorToInt(sourceRect.y);
+        int width = Mathf.FloorToInt(sourceRect.width);
+        int height = Mathf.FloorToInt(sourceRect.height);
+
+        
+
+
+        for (int i = 0; i < 40; i++)
+            for (int j = 0; j < 40; j++)
+                
+                {
+
+                Color pix2 = sourceTex.GetPixel(i, j  );
+
+                Vector3 NewPos = new Vector3(1.0f*i, 100, 1.0f * j);
+                    
+
+                    BoxClone = Instantiate(BulletPF, NewPos, transform.rotation);
+                    var color1 = (int)Random.Range(0, 255);
+                    var color2 = (int)Random.Range(0, 255);
+                    var color3 = (int)Random.Range(0, 255);
+                //                   var df = new UnityEngine.Color(color1 / 255.0f, color2 / 255.0f, color3 / 255.0f);
+                var ColorN = new UnityEngine.Color(color1 / 255.0f, color2 / 255.0f, color3 / 255.0f);
+               
+                
+                    var Rend2 = BoxClone.GetComponent<Renderer>();
+                    Rend2.material.SetColor("_Color", pix2);
+
+                    Destroy(BoxClone, 125.5f);
+                }
+
+
+
+
+        
+
+
+
+
+
+
+    }
+
+}
---
+++ Assets/scripts/RotatorXYZ.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RotatorXYZ : MonoBehaviour {
+	Quaternion originRotation;
+
+
+	float angle;
+	void Start () {
+		angle = 0;
+        originRotation=transform.rotation;
+
+
+    }
+
+	void FixedUpdate () {
+
+        angle++;
+        Quaternion rotationY = Quaternion.AngleAxis(angle, Vector3.up);
+        Quaternion rotationX = Quaternion.AngleAxis(angle, Vector3.right);
+
+        Quaternion rotationZ = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = originRotation * rotationY * rotationX* rotationZ;
+
+	
+	}
+}
